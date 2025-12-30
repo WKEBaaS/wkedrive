6a6d69937d904eb99992b40ea7e2fdaa1c7f6978
youmin1017 <youmin.main@gmail.com>
2025-12-30 21:36:29 +0800

    feat: add head for wkedrive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,18 @@
-<script lang="ts">
+<script lang='ts'>
 	import '@fontsource/noto-sans-tc';
-	import '@fontsource/open-sans';
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import { Toaster } from '$lib/components/ui/sonner';
-	import { ModeWatcher } from 'mode-watcher';
-	import '../app.css';
+  import '@fontsource/open-sans';
+  import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+  import { Toaster } from '$lib/components/ui/sonner';
+  import { ModeWatcher } from 'mode-watcher';
+  import '../app.css';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>WKE Drive</title>
+	<meta name='description' content='WKE BaaS Case Study' />
+</svelte:head>
 
 <ModeWatcher />
 <Toaster />
